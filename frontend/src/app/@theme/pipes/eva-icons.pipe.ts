24179aaef6c5b9af49ed7b5e490f704372527f9d
Caelin Sutch <caelinsutch@Caelins-MacBook-Pro.local>
2019-08-21 09:32:34 -0700

    Updated with some stuff, gitignore, changed some files

--- frontend/src/app/@theme/pipes/eva-icons.pipe.ts
+++ frontend/src/app/@theme/pipes/eva-icons.pipe.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) Akveo 2019. All Rights Reserved.
- * Licensed under the Single Application / Multi Application License.
- * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
- */
-
 import { DomSanitizer } from '@angular/platform-browser';
 import { Pipe, PipeTransform } from '@angular/core';
 import { icons } from 'eva-icons';
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) Akveo 2019. All Rights Reserved.
- * Licensed under the Single Application / Multi Application License.
- * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
- */
-
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthGuard } from './@auth/auth.guard';
@@ -11,7 +5,7 @@ import { AuthGuard } from './@auth/auth.guard';
 const routes: Routes = [
   {
     path: 'pages',
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     loadChildren: () => import('app/pages/pages.module')
       .then(m => m.PagesModule),
   },
