d69970e56dfb9faf0fea00ddea3e1d31b71b7be3
Alden Parker <ajparker1401@gmail.com>
2019-08-28 07:38:20 -0700

    fix button border and add get request to google sheets for submisions

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -10,6 +10,31 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz44ZiWQmppawIoxjVtCqvnuM44fPUgQnmpZtSASBQVXa8jL20/exec'
+    const form1 = document.forms['email1']
+    const form2 = document.forms['email2']
+    const form3 = document.forms['fullForm']
+
+    form1.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form1)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    });
+
+    form2.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form2)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    });
+
+    form3.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form3)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    });
   }
 
 }
