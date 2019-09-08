57ce0014c7a95376f3ff6b3023c47820f2e824dc
Caelin Sutch <Caelinsutch@gmail.com>
2019-09-08 10:06:59 -0700

    Major deletion of unneeded code, refactored Aldens sucky html

--- frontend/src/app/@core/backend/common/common-backend.module.ts
+++ frontend/src/app/@core/backend/common/common-backend.module.ts
@@ -4,9 +4,6 @@ import { UserData } from '../../interfaces/common/users';
 import { UsersService } from './services/users.service';
 import { UsersApi } from './api/users.api';
 import { HttpService } from './api/http.service';
-import { CountryData } from '../../interfaces/common/countries';
-import { CountriesService } from './services/countries.service';
-import { CountriesApi } from './api/countries.api';
 import { SettingsApi } from './api/settings.api';
 import { NbAuthModule } from '@nebular/auth';
 import { SettingsData } from '../../interfaces/common/settings';
@@ -15,11 +12,10 @@ import {ModelData} from '../../interfaces/common/model';
 import {ModelsService} from './services/models.service';
 import {ModelsApi} from './api/models.api';
 
-const API = [UsersApi, CountriesApi, SettingsApi,  HttpService, ModelsApi];
+const API = [UsersApi, SettingsApi,  HttpService, ModelsApi];
 
 const SERVICES = [
   { provide: UserData, useClass: UsersService },
-  { provide: CountryData, useClass: CountriesService },
   { provide: SettingsData, useClass: SettingsService },
   { provide: ModelData, useClass: ModelsService },
 ];
--- frontend/src/app/@core/mock/common/common-mock.module.ts
+++ frontend/src/app/@core/mock/common/common-mock.module.ts
@@ -1,14 +1,9 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SmartTableData } from '../../interfaces/common/smart-table';
-
-import { SmartTableService } from './smart-table.service';
 import { SettingsData } from '../../interfaces/common/settings';
 import { SettingsService } from './settings.service';
 
 const SERVICES = [
-  { provide: SmartTableData, useClass: SmartTableService },
   { provide: SettingsData, useClass: SettingsService },
 ];
 
